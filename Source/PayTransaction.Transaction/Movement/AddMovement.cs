3196f05fe4af4268038dcc6e8bfbb5f4fe85f03f
csvitor-dev <csvitor.dev@gmail.com>
2025-01-12 17:36:00 -0300

    refactor+feat: implement `AddMovement` transaction

--- Source/PayTransaction.Transaction/Movement/AddMovement.cs
+++ Source/PayTransaction.Transaction/Movement/AddMovement.cs
@@ -1,6 +1,21 @@
+using PayTransaction.Core.Data;
+
 namespace PayTransaction.Application.Movement;
 
-public abstract class AddMovement(int accountId, double amount)
+public abstract class AddMovement
+    (int accountId) : ITransaction
 {
+    protected abstract Core.Entities.Movement MakeMovement();
     
+    public void Execute()
+    {
+        var account = PayDb.GetAccount(accountId);
+        
+        if (account is null)
+            throw new InvalidOperationException("Account not found");
+        var movement = MakeMovement();
+        movement.Transact(account);
+        
+        account.AddMovement(movement);
+    }
 }
