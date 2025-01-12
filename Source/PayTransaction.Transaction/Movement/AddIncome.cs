dc9ea4ca228036654a4701381beabcf9a805dd9e
csvitor-dev <csvitor.dev@gmail.com>
2025-01-12 17:38:28 -0300

    feat: implement `AddIncome` transaction that extends `AddMovement`
    <SUMMARY> Note that the implementation of this class already follows the
    FACTORY METHOD design pattern.

---
+++ Source/PayTransaction.Transaction/Movement/AddIncome.cs
@@ -0,0 +1,9 @@
+using PayTransaction.Core.Financial;
+
+namespace PayTransaction.Application.Movement;
+
+public class AddIncome(int accountId, int incomeId, double amount) : AddMovement(accountId)
+{
+    protected override Core.Entities.Movement MakeMovement()
+        => new Income(incomeId, amount);
+}
