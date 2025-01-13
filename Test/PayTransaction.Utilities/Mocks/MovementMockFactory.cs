0459da7dd79c9cd14cc62d5017de517b1b19d10f
csvitor-dev <csvitor.dev@gmail.com>
2025-01-13 11:41:56 -0300

    feat: create mock to expense movement transaction

--- Test/PayTransaction.Utilities/Mocks/MovementMockFactory.cs
+++ Test/PayTransaction.Utilities/Mocks/MovementMockFactory.cs
@@ -5,12 +5,26 @@ namespace PayTransaction.Utils.Mocks;
 
 public abstract class MovementMockFactory : MockFactory
 {
-    public static (AddIncome, MovementExpected) CreateIncomeMock(int accountId)
+    private static (int, double) GetBaseData()
     {
         var id = Faker.Random.Number(1, 2_000);
         var amount = Faker.Random.Double(100, 1_000);
+
+        return (id, amount);
+    }
+    public static (AddIncome, MovementExpected) CreateIncomeMock(int accountId)
+    {
+        var (id, amount) = GetBaseData();
         var expected = new MovementExpected(id, amount);
         
         return (new AddIncome(accountId, id, amount), expected);
+    }
+    
+    public static (AddExpense, MovementExpected) CreateExpenseMock(int accountId)
+    {
+        var (id, amount) = GetBaseData();
+        var expected = new MovementExpected(id, amount);
+        
+        return (new AddExpense(accountId, id, amount), expected);
     }
 }
