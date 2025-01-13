2e64664b02657add14c76c1e9a13581c1bd7ed72
csvitor-dev <csvitor.dev@gmail.com>
2025-01-13 12:02:14 -0300

    test: create a test case when add a transfer movement transaction, test fail

--- Test/PayTransaction.Test/Base/AddMovementTest.cs
+++ Test/PayTransaction.Test/Base/AddMovementTest.cs
@@ -46,4 +46,25 @@ public class AddMovementTest
         Assert.That(account?.Balance,
             Is.EqualTo(expectedAccount.Balance - expectedMovement.Amount));
     }
+    
+    [Test]
+    public void Test_AddTransfer_OnSuccess()
+    {
+        var (accountTransaction, expectedAccount) = AccountMockFactory.CreateAccountMock();
+        var (expenseTransaction, expectedMovement) = MovementMockFactory
+            .CreateTransferMock(expectedAccount.Id);
+        
+        accountTransaction.Execute();
+        expenseTransaction.Execute();
+        var account = PayDb.GetAccount(expectedAccount.Id);
+        var movement = account?.GetMovement(expectedMovement.Id);
+        
+        Assert.That(account?.Movements, Is.Not.Empty);
+        Assert.That(account?.Wallet, Is.EqualTo(expectedMovement.Amount));
+        Assert.That(movement is Transfer, Is.True);
+        Assert.That(movement?.Id, Is.EqualTo(expectedMovement.Id));
+        Assert.That(movement?.Amount, Is.EqualTo(expectedMovement.Amount));
+        Assert.That(account.Balance,
+            Is.EqualTo(expectedAccount.Balance - expectedMovement.Amount));
+    }
 }
