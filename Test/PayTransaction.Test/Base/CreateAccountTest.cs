65b1b81c6aeb27947bdf77a53ea24a5c5f125517
csvitor-dev <csvitor.dev@gmail.com>
2025-01-13 14:45:51 -0300

    test: create a test case when create a investment wallet, test fail
    <DETAILS> this test takes priority over ther previous test
    (`Test_AddTransfer_OnSuccess`)

--- Test/PayTransaction.Test/Base/CreateAccountTest.cs
+++ Test/PayTransaction.Test/Base/CreateAccountTest.cs
@@ -19,4 +19,22 @@ public class CreateAccountTest
         Assert.That(account.CreatedOn, Is.EqualTo(expected.CreatedOn));
         Assert.That(account.Balance, Is.EqualTo(expected.Balance));
     }
+
+    [Test]
+    public void Test_AddInvestmentWallet_OnSuccess()
+    {
+        var (accountTransaction, expectedAccount) = AccountMockFactory.CreateAccountMock();
+        var (walletTransaction, expectedWallet) = WalletMockFactory
+            .CreateWalletMock(expectedAccount.Id);
+
+        accountTransaction.Execute();
+        walletTransaction.Execute();
+        var account = PayDb.GetAccount(expectedAccount.Id);
+        var wallet = PayDb.GetWallet(expectedAccount.Id);
+
+        Assert.That(wallet, Is.Not.Null);
+        Assert.That(wallet.Movements, Is.Empty);
+        Assert.That(wallet.AccountId, Is.EqualTo(expectedAccount.Id));
+        Assert.That(account.Wallet, Is.EqualTo(expectedWallet.Balance));
+    }
 }
