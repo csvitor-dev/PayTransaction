c0b37e3670debdb3cb968441ade799c8af5c3a70
csvitor-dev <csvitor.dev@gmail.com>
2025-01-14 11:10:50 -0300

    feat: apply the ADAPTER pattern that respects the expected interface

---
+++ Source/PayTransaction.Transaction/Services/Payment/PaypalServiceAdapter.cs
@@ -0,0 +1,19 @@
+using PayTransaction.Core.Contracts;
+
+namespace PayTransaction.Application.Services.Payment;
+
+public class PaypalServiceAdapter // ADAPTER
+    (PaypalService service) : IPaymentService
+{
+    public void ProcessPayment(Core.Entities.Payment payment)
+    {
+        var paymentId = payment.Id.ToString();
+        var amount = Convert.ToDecimal(payment.Amount);
+        
+        service.Pay(paymentId, amount);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Payment successfully processed.");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine(payment.Description);
+    }
+}
