4b6b676dbfd59f3dcb075d495c22032ca899fdcf
csvitor-dev <csvitor.dev@gmail.com>
2025-01-14 11:28:48 -0300

    feat: create a statement flow for ADAPTER demonstration

---
+++ Source/PayTransaction.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using PayTransaction.Application.Services.Payment;
+using PayTransaction.Core.Contracts;
+using PayTransaction.Core.Entities;
+
+var payment = new Payment(238,
+    "compra do livro 'Padrões de Projetos: Soluções Reutilizáveis de Software Orientados a Objetos'",
+    148.5);
+
+var paypal = new PaypalService();
+IPaymentService adapter = new PaypalServiceAdapter(paypal);
+
+adapter.ProcessPayment(payment);
