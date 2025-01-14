8cbe98d77c31a0f949e849f80f71f4d05544a20c
csvitor-dev <csvitor.dev@gmail.com>
2025-01-13 21:22:51 -0300

    refactor: rename from `expenseTransaction` to `transferTransaction` and some improvements

--- Test/PayTransaction.Test/Base/AddMovementTest.cs
+++ Test/PayTransaction.Test/Base/AddMovementTest.cs
@@ -51,19 +51,23 @@ public class AddMovementTest
     public void Test_AddTransfer_OnSuccess()
     {
         var (accountTransaction, expectedAccount) = AccountMockFactory.CreateAccountMock();
-        var (expenseTransaction, expectedMovement) = MovementMockFactory
+        var (transferTransaction, expectedMovement) = MovementMockFactory
             .CreateTransferMock(expectedAccount.Id);
         
         accountTransaction.Execute();
-        expenseTransaction.Execute();
+        transferTransaction.Execute();
         var account = PayDb.GetAccount(expectedAccount.Id);
-        var movement = account?.GetMovement(expectedMovement.Id);
+        var wallet = PayDb.GetWallet(expectedAccount.Id);
+        var accountMovement = account?.GetMovement(expectedMovement.Id);
+        var walletMovement = wallet?.GetMovement(expectedMovement.Id);
+        
         
         Assert.That(account?.Movements, Is.Not.Empty);
-        Assert.That(account?.Wallet, Is.EqualTo(expectedMovement.Amount));
-        Assert.That(movement is Transfer, Is.True);
-        Assert.That(movement?.Id, Is.EqualTo(expectedMovement.Id));
-        Assert.That(movement?.Amount, Is.EqualTo(expectedMovement.Amount));
+        Assert.That(wallet?.Balance, Is.EqualTo(expectedMovement.Amount));
+        Assert.That(accountMovement is Transfer, Is.True);
+        Assert.That(walletMovement, Is.Not.Null);
+        Assert.That(accountMovement?.Id, Is.EqualTo(expectedMovement.Id));
+        Assert.That(accountMovement?.Amount, Is.EqualTo(expectedMovement.Amount));
         Assert.That(account.Balance,
             Is.EqualTo(expectedAccount.Balance - expectedMovement.Amount));
     }
