cf5959206af9bf997b292df34ab3bd0653b4422c
csvitor-dev <csvitor.dev@gmail.com>
2025-01-13 21:51:39 -0300

    refactor: improve `InvestmentWallet` properties and methods

--- Source/PayTransaction.Core/Entities/InvestmentWallet.cs
+++ Source/PayTransaction.Core/Entities/InvestmentWallet.cs
@@ -2,7 +2,22 @@ namespace PayTransaction.Core.Entities;
 
 public class InvestmentWallet(int accountId, double balance)
 {
+    private readonly List<Movement> _movements = [];
+    
     public int AccountId { get; } = accountId;
     public double Balance { get; private set; } = balance;
-    public string[] Movements { get; } = [];
+    
+    public Movement[] Movements => _movements.ToArray();
+    
+    public void Deposit(double amount)
+        => Balance += amount;
+    
+    public void Withdraw(double amount)
+        => Balance -= amount;
+    
+    public void AddMovement(Movement movement)
+        => _movements.Add(movement);
+    
+    public Movement? GetMovement(int movementId)
+        => _movements.FirstOrDefault(movement => movement.Id == movementId);
 }
